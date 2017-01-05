43abeb286d035fa1a58691f1322999b8ad22ec40
Manuel Wildauer <m.wildauer@gmail.com>
2017-01-05 23:09:22 +0100

    Inital

---
+++ README.md
@@ -0,0 +1,17 @@
+# i3blocks-battery
+
+This is a fork of the [battery blocklet](https://github.com/vivien/i3blocks/blob/master/scripts/battery) written in Rust.
+
+### Build
+```
+$ cargo build
+```
+
+### Add to i3blocks.conf
+```
+[battery]
+label=
+command=/path/to/i3blocks-battery
+interval=30
+color=#ffffff
+```
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::path::Path;
+use std::process;
+
+fn main() {
+
+    let battery = "BAT0";
+
+    let color_bad = "#FF0000";
+    let color_yellow = "#FFAE00";
+    let color_ok = "#A8FF00";
+
+    /**
+     * Build bath to 'battery'
+     */
+    let path = Path::new("/sys/class/power_supply");
+    let base_path = path.join(battery);
+
+    let mut capacity = capacity(base_path.clone()).to_string();
+    let len_capacity = capacity.len();
+    capacity.truncate(len_capacity - 1);
+
+    let mut status = status(base_path.clone()).to_string();
+    let len_status = status.len();
+    status.truncate(len_status - 1);
+
+    if status == String::from("Discharging") {
+        status = "DIS".to_string();
+    }
+
+    println!("{}% {}", capacity, status);
+    println!("{}% {}", capacity, status);
+
+    let mut color = String::new();
+    if capacity < String::from("20") {
+        color.push_str(color_bad);
+    } else if capacity < String::from("50") {
+        color.push_str(color_yellow);
+    } else {
+        color.push_str(color_ok);
+    }
+
+    println!("{}", color);
+
+    /*
+     * set to urgent when capacity < 5%
+     */
+    if capacity < String::from("5") {
+        process::exit(33);
+    }
+}
+
+/**
+ * read battery capacity
+ */
+fn capacity(base_path: PathBuf) -> String {
+    let mut path = PathBuf::from(base_path);
+    path.push("capacity");
+    return get_information(path);
+}
+
+/**
+ * read battery capacity
+ */
+fn status(base_path: PathBuf) -> String {
+    let mut path = PathBuf::from(base_path);
+    path.push("status");
+    return get_information(path);
+}
+
+/**
+ * get information from file
+ */
+fn get_information(path: PathBuf) -> String {
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
+        Ok(file) => file,
+    };
+
+    let mut information = String::new();
+    match file.read_to_string(&mut information) {
+        Err(why) => panic!("couldn't read {}: {}", path.display(), why.description()),
+        Ok(_) => return information,
+    }
+}
