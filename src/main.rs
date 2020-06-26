ab7a4d0b7b7fe8b2fdc9b34237b0e71683f3da2d
Reiner Herrmann <reiner@reiner-h.de>
2020-06-26 15:46:39 +0200

    Some simplifications and linter fixes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
-use std::path::Path;
 use std::process;
 
 fn main() {
@@ -13,61 +11,50 @@ fn main() {
     let color_yellow = "#FFAE00";
     let color_ok = "#A8FF00";
 
-    /**
+    /*
      * Build bath to 'battery'
      */
-    let path = Path::new("/sys/class/power_supply");
-    let base_path = path.join(battery);
+    let base_path = PathBuf::from(format!("/sys/class/power_supply/{}", battery));
 
-    let mut capacity = get_value(base_path.clone(), "capacity".to_string()).to_string();
-    let len_capacity = capacity.len();
-    capacity.truncate(len_capacity - 1);
+    let capacity = match get_value(&base_path, "capacity").parse::<u32>() {
+        Ok(c) => c,
+        Err(e) => panic!("capacity is no integer: {}", e),
+    };
 
-    let mut status = get_value(base_path.clone(), "status".to_string()).to_string();
-    let len_status = status.len();
-    status.truncate(len_status - 1);
-
-    if status == String::from("Discharging") {
-        status = "DIS".to_string();
-    }
+    let status = get_value(&base_path, "status");
+    let status = match status.as_str() {
+        "Discharging" => "DIS",
+        _ => &status,
+    };
 
     println!("{}% {}", capacity, status);
     println!("{}% {}", capacity, status);
 
-    let mut color = String::new();
-    if capacity < String::from("20") {
-        color.push_str(color_bad);
-    } else if capacity < String::from("50") {
-        color.push_str(color_yellow);
-    } else {
-        color.push_str(color_ok);
-    }
+    let color = match capacity {
+        _ if capacity < 10 => color_bad,
+        _ if capacity < 50 => color_yellow,
+        _ => color_ok
+    };
 
     println!("{}", color);
 
     /*
      * set to urgent when capacity < 5%
      */
-    if capacity < String::from("5") {
+    if capacity < 5 {
         process::exit(33);
     }
 }
 
-fn get_value(base_path: PathBuf, value: String) -> String {
+fn get_value(base_path: &PathBuf, value: &str) -> String {
     let mut path = PathBuf::from(base_path);
     path.push(value);
-    return get_information(path);
+    get_information(&path).trim_end().to_string()
 }
 
-fn get_information(path: PathBuf) -> String {
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
-        Ok(file) => file,
-    };
-
+fn get_information(path: &PathBuf) -> String {
+    let mut file = File::open(&path).unwrap_or_else(|_| panic!("couldn't open {}", path.display()));
     let mut information = String::new();
-    match file.read_to_string(&mut information) {
-        Err(why) => panic!("couldn't read {}: {}", path.display(), why.description()),
-        Ok(_) => return information,
-    }
+    file.read_to_string(&mut information).unwrap_or_else(|_| panic!("couldn't read {}", path.display()));
+    information
 }
